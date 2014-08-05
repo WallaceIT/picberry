b272128212f943fac511b25795655a50c4045901
WallaceIT <valla.francesco@gmail.com>
2014-08-05 11:49:15 +0200

    dspic33e: Added device informations

--- dspic33e.h
+++ dspic33e.h
@@ -46,5 +46,16 @@ class dspic33e : public Pic{
 		* DEVICES SECTION
 		*                       ID       NAME           	  MEMSIZE
 		*/
-		pic_device piclist[1] = {{0x1861, "DSPIC33EP256MU806", 0x02ABFF}};
+		pic_device piclist[12] = {{0x1861, "dsPIC33EP256MU806", 0x02ABFF},
+								  {0x1862, "dsPIC33EP256MU810", 0x02ABFF},
+								  {0x1863, "dsPIC33EP256MU814", 0x02ABFF},
+								  {0x1826, "PIC24EP256GU810", 0x02ABFF},
+								  {0x1827, "PIC24EP256GU814", 0x02ABFF},
+								  {0x187D, "dsPIC33EP512GP806", 0x02ABFF},
+								  {0x1879, "dsPIC33EP512MC806", 0x0557FF},
+								  {0x1872, "dsPIC33EP512MU810", 0x0557FF},
+								  {0x1873, "dsPIC33EP512MU814", 0x0557FF},
+								  {0x183D, "PIC24EP512GP806", 0x0557FF},
+								  {0x1836, "PIC24EP512GU810", 0x0557FF},
+								  {0x1837, "PIC24EP512GU814", 0x0557FF}};
 };
